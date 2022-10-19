9b27ed365e81581e3d3c70445d814adbad32bafd
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-10-18 20:55:37 -0400

    separating each nav as different pages

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
-import About from './components/About';
-import Contact from './components/Contact';
-import Gallery from './components/Gallery';
-import Modal from './components/Modal';
-import Nav from './components/Nav';
-import PhotoList from './components/PhotoList';
-import ContactForm from './components/Contact';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import Gallery from './components/Gallery';
+// import Modal from './components/Modal';
+// import Nav from './components/Nav';
+// import PhotoList from './components/PhotoList';
+// import ContactForm from './components/Contact';
+import PortfolioContainer from "./components/PortfolioContainer";
 
 
-function App() {
-  const [categories] = useState([
-    {
-      name: 'commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
-  ]);
 
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+const App = () => <PortfolioContainer />;
 
-  const [contactSelected, setContactSelected] = useState(false);
 
-  return (
-    <div>
-      <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-        contactSelected={contactSelected}
-        setContactSelected={setContactSelected}
-      ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-            <Gallery currentCategory={currentCategory}></Gallery>
-            <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
-      </main>
-    </div>
-  );
-}
+
+// function App() {
+//   const [categories] = useState([
+//     { name: 'portfolio'},
+//     { name: 'resume', 
+//       description: 'Front-end Proficiencies'
+//     },
+//   ]);
+
+//   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+//   const [contactSelected, setContactSelected] = useState(false);
+
+//   return (
+//     <div>
+//       <Nav
+//         categories={categories}
+//         setCurrentCategory={setCurrentCategory}
+//         currentCategory={currentCategory}
+//         contactSelected={contactSelected}
+//         setContactSelected={setContactSelected}
+//       ></Nav>
+//       <main>
+//         {!contactSelected ? (
+//           <>
+//             <Gallery currentCategory={currentCategory}></Gallery>
+//             <About></About>
+//           </>
+//         ) : (
+//           <ContactForm></ContactForm>
+//         )}
+//       </main>
+//     </div>
+//   );
+// }
 
 export default App;

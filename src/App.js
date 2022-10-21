8e03f194400605abc5b6706f27592edd0221cf93
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-10-20 21:04:37 -0400

    removed unused code

--- src/App.js
+++ src/App.js
@@ -10,45 +10,6 @@ import './App.css';
 import PortfolioContainer from "./components/PortfolioContainer";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 const App = () => <PortfolioContainer />;
-
-
-
-// function App() {
-//   const [categories] = useState([
-//     { name: 'portfolio'},
-//     { name: 'resume', 
-//       description: 'Front-end Proficiencies'
-//     },
-//   ]);
-
-//   const [currentCategory, setCurrentCategory] = useState(categories[0]);
-
-//   const [contactSelected, setContactSelected] = useState(false);
-
-//   return (
-//     <div>
-//       <Nav
-//         categories={categories}
-//         setCurrentCategory={setCurrentCategory}
-//         currentCategory={currentCategory}
-//         contactSelected={contactSelected}
-//         setContactSelected={setContactSelected}
-//       ></Nav>
-//       <main>
-//         {!contactSelected ? (
-//           <>
-//             <Gallery currentCategory={currentCategory}></Gallery>
-//             <About></About>
-//           </>
-//         ) : (
-//           <ContactForm></ContactForm>
-//         )}
-//       </main>
-//     </div>
-//   );
-// }
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav({ currentPage, handlePageChange }) {
 
@@ -12,17 +11,6 @@ function Nav({ currentPage, handlePageChange }) {
         </a>
       </h2>
       <ul className="flex-row">
-        {/* <li className="mx-2">
-          <a
-            href="#home"
-            onClick={() => handlePageChange('Home')}
-            // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-            // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-            className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-          >
-            Home
-          </a>
-        </li> */}
         <li className="mx-2">
           <a
             href="#about"
@@ -66,57 +54,6 @@ function Nav({ currentPage, handlePageChange }) {
       </ul>
     </header>
   );
-
-  // const {
-  //   categories = [],
-  //   setCurrentCategory,
-  //   contactSelected,
-  //   currentCategory,
-  //   setContactSelected,
-  // } = props;
-
-  // useEffect(() => {
-  //   document.title = capitalizeFirstLetter(currentCategory.name);
-  // }, [currentCategory]);
-
-  // return (
-  //   <header className="flex-row px-1">
-  //     <h2>
-  //       <a data-testid="link" href="/">
-  //         <span role="img" aria-label="camera"> 📸</span> Oh Snap!
-  //       </a>
-  //     </h2>
-  //     <nav>
-  //       <ul className="flex-row">
-  //         <li className="mx-2">
-  //           <a data-testid="about" href=" " onClick={() => setContactSelected(false)}>
-  //             About me
-  //           </a>
-  //         </li>
-  //         <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-  //           <span onClick={() => setContactSelected(true)}>Contact</span>
-  //         </li>
-  //         {categories.map((category) => (
-  //           <li
-  //             className={`mx-1 ${
-  //               currentCategory.name === category.name && !contactSelected && 'navActive'
-  //               }`}
-  //             key={category.name}
-  //           >
-  //             <span
-  //               onClick={() => {
-  //                 setCurrentCategory(category);
-  //                 setContactSelected(false);
-  //               }}
-  //             >
-  //               {capitalizeFirstLetter(category.name)}
-  //             </span>
-  //           </li>
-  //         ))}
-  //       </ul>
-  //     </nav>
-  //   </header>
-  // );
 }
 
 export default Nav;

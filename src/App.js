f83f1d4eea50d88ae788d7fd03a52ff80050867f
Morgan Gilbert <morgan.el.gilbert@gmail.com>
2022-10-20 22:55:18 -0400

    cleaned up code, removed unused files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
 import './App.css';
-// import About from './components/About';
-// import Contact from './components/Contact';
-// import Gallery from './components/Gallery';
-// import Modal from './components/Modal';
-// import Nav from './components/Nav';
-// import PhotoList from './components/PhotoList';
-// import ContactForm from './components/Contact';
 import PortfolioContainer from "./components/PortfolioContainer";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
